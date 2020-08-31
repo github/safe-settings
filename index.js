67d1f42757a26303ef35b41615bda87932531e87
Yadhav Jayaraman <57544838+decyjphr@users.noreply.github.com>
2020-08-31 00:06:27 -0400

    added code for safe settings

--- index.js
+++ index.js
@@ -1,18 +1,114 @@
 const mergeArrayByName = require('./lib/mergeArrayByName')
+const path = require('path')
+const yaml = require('js-yaml')
+/*
+const default_config = {
+  labels: [
+    {
+      name: 'bug',
+      color: 'CC0000',
+      description: 'An issue with the system'
+    },
+    {
+      name: 'feature',
+      color: '#336699',
+      description: 'New functionality.'
+    },
+    {
+      name: 'first-timers-only',
+      oldname: 'Help Wanted',
+      color: '#326699'
+    },
+    { name: 'new-label', oldname: 'Help Wanted', color: '#326699' }
+  ],
+  collaborators: [ { username: 'regpaco', permission: 'pull' } ],
+  teams: [
+    { name: 'core', permission: 'admin' },
+    { name: 'docss', permission: 'push' },
+    { name: 'docs', permission: 'pull' }
+  ],
+  branches: [
+    {
+      name: 'main-enterprise',
+      protection: {
+        required_pull_request_reviews: [Object],
+        required_status_checks: [Object],
+        enforce_admins: true,
+        restrictions: [Object]
+      }
+    }
+  ]
+}
+*/
 
 module.exports = (robot, _, Settings = require('./lib/settings')) => {
+  async function syncAllSettings (context, repo = context.repo()) {
+    //const config = await context.config('settings.yml', default_config, { arrayMerge: mergeArrayByName })
+    const config = await loadYaml(context)
+    robot.log(`config is ${JSON.stringify(config)}`)
+    return Settings.syncAll(context, repo, config)
+  }
+
   async function syncSettings (context, repo = context.repo()) {
-    const config = await context.config('settings.yml', {}, { arrayMerge: mergeArrayByName })
-    return Settings.sync(context.github, repo, config)
+    //const config = await context.config('settings.yml', default_config, { arrayMerge: mergeArrayByName })
+    //const config = await context.config('settings.yml')
+    const config = await loadYaml(context)
+    robot.log(`config is ${JSON.stringify(config)}`)
+    return Settings.sync(context, repo, config)
+  }
+
+    /**
+   * Loads a file from GitHub
+   *
+   * @param params Params to fetch the file with
+   * @return The parsed YAML file
+   */
+  async function loadYaml (context) {
+    try {
+      const repo = {owner: 'decyjphr-org', repo: 'admin'}
+      const CONFIG_PATH = '.github'
+      const params = Object.assign(repo,{ path: path.posix.join(CONFIG_PATH, 'settings.yml') })
+      const response = await context.github.repos.getContents(params).catch(e=>{
+        console.log(e)
+        console.error(`Error getting settings ${e}`)
+      })
+      //console.log(response)
+      // Ignore in case path is a folder
+      // - https://developer.github.com/v3/repos/contents/#response-if-content-is-a-directory
+      if (Array.isArray(response.data)) {
+        return null
+      }
+
+      // we don't handle symlinks or submodule
+      // - https://developer.github.com/v3/repos/contents/#response-if-content-is-a-symlink
+      // - https://developer.github.com/v3/repos/contents/#response-if-content-is-a-submodule
+      if (typeof response.data.content !== 'string') {
+        return
+      }
+
+      return yaml.safeLoad(Buffer.from(response.data.content, 'base64').toString()) || {}
+    } catch (e) {
+      if (e.status === 404) {
+        return null
+      }
+
+      throw e
+    }
   }
 
   robot.on('push', async context => {
     const { payload } = context
     const { repository } = payload
 
+    const adminRepo =  repository.name === 'admin'
+    if (!adminRepo) {
+      robot.log('Not working on the Admin repo, returning...')
+      return
+    }
+
     const defaultBranch = payload.ref === 'refs/heads/' + repository.default_branch
     if (!defaultBranch) {
-      robot.log.debug('Not working on the default branch, returning...')
+      robot.log('Not working on the default branch, returning...')
       return
     }
 
@@ -22,28 +118,42 @@ module.exports = (robot, _, Settings = require('./lib/settings')) => {
     })
 
     if (!settingsModified) {
-      robot.log.debug(`No changes in '${Settings.FILE_NAME}' detected, returning...`)
+      robot.log(`No changes in '${Settings.FILE_NAME}' detected, returning...`)
       return
     }
 
-    return syncSettings(context)
+    return syncAllSettings(context)
   })
 
   robot.on('repository.edited', async context => {
     const { payload } = context
-    const { changes, repository } = payload
-
+    const { changes, repository, sender } = payload
+    robot.log("repository.edited payload from ",JSON.stringify(sender))
+    if (sender.type === 'Bot') {
+      robot.log('Repository Edited by a Bot')
+      return
+    }
+    console.log('Repository Edited by a Human')
     if (!Object.prototype.hasOwnProperty.call(changes, 'default_branch')) {
-      robot.log.debug('Repository configuration was edited but the default branch was not affected, returning...')
+      robot.log('Repository configuration was edited but the default branch was not affected, returning...')
       return
     }
 
-    robot.log.debug(`Default branch changed from '${changes.default_branch.from}' to '${repository.default_branch}'`)
+    robot.log(`Default branch changed from '${changes.default_branch.from}' to '${repository.default_branch}'`)
 
     return syncSettings(context)
   })
 
   robot.on('repository.created', async context => {
+    const { payload } = context
+    const { changes, repository, sender } = payload
+    robot.log("repository.created payload from ",JSON.stringify(sender))
+    if (sender.type === 'Bot') {
+      robot.log('Repository created by a Bot')
+      return
+    }
+    robot.log('Repository created by a Human')
     return syncSettings(context)
   })
+
 }
