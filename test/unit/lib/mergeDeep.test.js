06094b0fbc2e0e1830eaf49f4b8cdeb5399a69a6
Johan Andersson <johan.andersson@lime.tech>
2023-01-04 20:39:05 +0100

    chore: eslint --fix

--- test/unit/lib/mergeDeep.test.js
+++ test/unit/lib/mergeDeep.test.js
@@ -8,12 +8,12 @@ describe('MergeDeep Tests', () => {
     const target = YAML.load(`
 repository:
   # A short description of the repository that will show up on GitHub
-  description: description of the repos 
-      
+  description: description of the repos
+
   # A comma-separated list of topics to set on the repository
-  topics: 
-  - uber 
-  - newone 
+  topics:
+  - uber
+  - newone
 branches:
   # If the name of the branch is default, it will create a branch protection for the default branch in the repo
   - name: default
@@ -48,40 +48,40 @@ branches:
         `)
 
     const source = YAML.load(`
-  repository:      
-    name: test   
+  repository:
+    name: test
     org: decyjphr-org
-    force_create: false 
+    force_create: false
     description: description of test repository
     homepage: https://newhome.github.io/
-    topics: 
-    - red  
-    auto_init: true  
-    has_issues: true 
+    topics:
+    - red
+    auto_init: true
+    has_issues: true
     has_projects: true
     has_wiki: false
     has_downloads: true
     allow_squash_merge: true
     allow_merge_commit: false
     allow_rebase_merge: false
-    default_branch: develop  
-  
-  labels:  
+    default_branch: develop
+
+  labels:
     # Labels: define labels for Issues and Pull Requests
     - name: green
       color: '#B60205'
-      description: An issue sswithss the system 
-      
+      description: An issue sswithss the system
+
   validator:
-    #pattern: '[a-zA-Z0-9_-]+_[a-zA-Z0-9_-]+.*' 
-    pattern: '[a-zA-Z0-9_-]+'  
-    
+    #pattern: '[a-zA-Z0-9_-]+_[a-zA-Z0-9_-]+.*'
+    pattern: '[a-zA-Z0-9_-]+'
+
   collaborators:
   - username: regpaco
     permission: pull
-  
-  branches:      
-    - name: feature1 
+
+  branches:
+    - name: feature1
       # https://developer.github.com/v3/repos/branches/#update-branch-protection
       # Branch Protection settings. Set to null to disable
       protection:
@@ -108,8 +108,8 @@ branches:
         # Required. Restrict who can push to this branch. Team and user restrictions are only available for organization-owned repositories. Set to null to disable.
         restrictions:
           apps: []
-          users: [] 
-          teams: []    
+          users: []
+          teams: []
         `)
 
     const expected = {
@@ -204,12 +204,12 @@ branches:
     const target = YAML.load(`
 repository:
   # A short description of the repository that will show up on GitHub
-  description: description of the repos 
-      
+  description: description of the repos
+
   # A comma-separated list of topics to set on the repository
-  topics: 
-  - uber 
-  - newone 
+  topics:
+  - uber
+  - newone
 branches:
   # If the name of the branch is default, it will create a branch protection for the default branch in the repo
   - name: default
@@ -244,40 +244,40 @@ branches:
         `)
 
     const source = YAML.load(`
-  repository:      
-    name: test   
+  repository:
+    name: test
     org: decyjphr-org
-    force_create: false 
+    force_create: false
     description: description of test repository
     homepage: https://newhome.github.io/
-    topics: 
-    - red  
-    auto_init: true  
-    has_issues: true 
+    topics:
+    - red
+    auto_init: true
+    has_issues: true
     has_projects: true
     has_wiki: false
     has_downloads: true
     allow_squash_merge: true
     allow_merge_commit: false
     allow_rebase_merge: false
-    default_branch: develop  
-  
-  labels:  
+    default_branch: develop
+
+  labels:
     # Labels: define labels for Issues and Pull Requests
     - name: green
       color: '#B60205'
-      description: An issue sswithss the system 
-      
+      description: An issue sswithss the system
+
   validator:
-    #pattern: '[a-zA-Z0-9_-]+_[a-zA-Z0-9_-]+.*' 
-    pattern: '[a-zA-Z0-9_-]+'  
-    
+    #pattern: '[a-zA-Z0-9_-]+_[a-zA-Z0-9_-]+.*'
+    pattern: '[a-zA-Z0-9_-]+'
+
   collaborators:
   - username: regpaco
     permission: pull
-  
-  branches:      
-    - name: feature1 
+
+  branches:
+    - name: feature1
       # https://developer.github.com/v3/repos/branches/#update-branch-protection
       # Branch Protection settings. Set to null to disable
       protection:
@@ -304,8 +304,8 @@ branches:
         # Required. Restrict who can push to this branch. Team and user restrictions are only available for organization-owned repositories. Set to null to disable.
         restrictions:
           apps: []
-          users: [] 
-          teams: []    
+          users: []
+          teams: []
         `)
 
     const expected = {
@@ -393,7 +393,7 @@ branches:
     expect(same.additions).toEqual({})
     expect(same.modifications).toEqual({})
   })
-  
+
 
   it('CompareDeep Undefined target Works', () => {
     const source = YAML.load(`
@@ -410,7 +410,7 @@ branches:
         `)
 
     const expected = {
-      additions: 
+      additions:
         {
           protection: {
             required_pull_request_reviews: {
@@ -479,12 +479,12 @@ branches:
       const merged = mergeDeep.compareDeep({}, source)
       expect(merged.additions).toEqual(expected.additions)
       expect(merged.modifications.length).toEqual(expected.modifications.length)
-  
+
       const overrideConfig = mergeDeep.mergeDeep({}, {}, source)
       const same = mergeDeep.compareDeep(overrideConfig, source)
       expect(same.additions).toEqual({})
       expect(same.modifications).toEqual({})
-  })   
+  })
 */
   it('CompareDeep Test when target is from the api', () => {
     const protection = {
@@ -607,7 +607,7 @@ branches:
   it('Merge labels with ignorable extra info Works', () => {
     const source = { entries: [{"id":3954990840,"node_id":"LA_kwDOHC6_Gc7rvF74","url":"https://api.github.com/repos/decyjphr-org/test2/labels/bug","name":"bug","color":"CC0000","default":true,"description":"An issue with the system"},{"id":4015763857,"node_id":"LA_kwDOHC6_Gc7vW7GR","url":"https://api.github.com/repos/decyjphr-org/test2/labels/feature","name":"feature","color":"336699","default":false,"description":"New functionality."},{"id":4015763934,"node_id":"LA_kwDOHC6_Gc7vW7He","url":"https://api.github.com/repos/decyjphr-org/test2/labels/first-timers-only","name":"first-timers-only","color":"326699","default":false,"description":null},{"id":4015763984,"node_id":"LA_kwDOHC6_Gc7vW7IQ","url":"https://api.github.com/repos/decyjphr-org/test2/labels/new-label","name":"new-label","color":"326699","default":false,"description":null}]}
     const target = { entries: [{"name":"bug","color":"CC0000","description":"An issue with the system"},{"name":"feature","color":"336699","description":"New functionality."},{"name":"first-timers-only","oldname":"Help Wanted","color":"326699"},{"name":"new-label","oldname":"Help Wanted","color":"326699"}]}
-  
+
 
     const expected = {
       additions: {},
@@ -629,24 +629,24 @@ branches:
     expect(same.additions).toEqual({})
     expect(same.modifications).toEqual({})
   })
-  
 
-  it('Merge labels ', () => { 
+
+  it('Merge labels ', () => {
     const source = YAML.load(`
     repository:
       name: new
       home: new home
-    labels:  
+    labels:
     # Labels: define labels for Issues and Pull Requests
     - name: green
       color: '#B60205'
-      description: An issue sswithss the system     
-    
+      description: An issue sswithss the system
+
     `)
     const target = YAML.load(`
   repository:
     name: new
-    home: old home  
+    home: old home
     `)
     const expected = {
         additions: {
@@ -677,12 +677,12 @@ branches:
     expect(same.additions).toEqual({})
     expect(same.modifications).toEqual({})
   })
-   
+
 
   it('Merge arrays deep', () => {
     const source = [{name: "blue", color: "green"},{name: "newone",color: "red"},{ name: "uber",color: "yellow"}]
     const target = [{name: "blue", color: "blue"}]
-  
+
     const expected = {
       additions:[{name:"newone",color:"red"},{name:"uber",color:"yellow"}],
       modifications:[{color:"green",name:"blue"}],
@@ -708,7 +708,7 @@ branches:
   it('Merge array of topics', () => {
     const source = { entries: ["blue","green","newone","red","uber","yellow"]}
     const target = { entries: ["red","blu"]}
-  
+
 
     const expected = {
       additions: {
@@ -728,7 +728,7 @@ branches:
     expect(same.additions).toEqual({})
     expect(same.modifications).toEqual({})
   })
-  
+
   it('Merge branch protections with context sub arrays', () => {
     const source = YAML.load(`
   branches:
@@ -749,7 +749,7 @@ branches:
       block_creations: true
       required_conversation_resolution: true
       `)
-    
+
     const target = YAML.load(`
     branches:
     - name: default
@@ -757,9 +757,9 @@ branches:
         required_status_checks:
           strict: true
           contexts:
-            - "Lint, compile and build"    
+            - "Lint, compile and build"
     `)
-  
+
     const expected = {
       additions:{},
       modifications: {
@@ -805,20 +805,20 @@ branches:
     const source = YAML.load(`
   branches:
   - name: default
-    values: 
+    values:
       a: [a,b,c]
   - name: new
-    values: 
+    values:
       a: [b]
       `)
-    
+
     const target = YAML.load(`
     branches:
     - name: default
-      values: 
-        a: [c,a]    
+      values:
+        a: [c,a]
     `)
-  
+
     const expected = JSON.parse(`{"additions":{"branches":[{"name":"new","values":{"a":["b"]}}]},"modifications":{"branches":[{"values":{"a":["b"]},"name":"default"}]},"hasChanges":true}`)
     const ignorableFields = []
     const mergeDeep = new MergeDeep(log, ignorableFields)
@@ -835,40 +835,40 @@ branches:
     expect(same.additions).toEqual({})
     expect(same.modifications).toEqual({})
   })
-  
+
   it('Repo test', () => {
 
     const source = YAML.load(`
-  repository:      
-    name: test   
+  repository:
+    name: test
     org: decyjphr-org
-    force_create: false 
+    force_create: false
     description: description of test repository
     homepage: https://newhome.github.io/
-    topics: 
-    - red  
-    auto_init: true  
-    has_issues: true 
+    topics:
+    - red
+    auto_init: true
+    has_issues: true
     has_projects: true
     has_wiki: false
     has_downloads: true
     allow_squash_merge: true
     allow_merge_commit: false
     allow_rebase_merge: false
-    default_branch: develop  
+    default_branch: develop
         `)
 
     const target = YAML.load(`
   repository:
     # A short description of the repository that will show up on GitHub
-    description: description of the repos 
-      
+    description: description of the repos
+
     # A comma-separated list of topics to set on the repository
-    topics: 
-    - uber 
-    - newone 
+    topics:
+    - uber
+    - newone
         `)
-  
+
     const expected = {
       additions: {
         repository: {
@@ -913,7 +913,7 @@ branches:
     expect(same.modifications).toEqual({})
 
   })
-  
+
   it('Autolinks test', () => {
     const source = YAML.load(`
 entries:
