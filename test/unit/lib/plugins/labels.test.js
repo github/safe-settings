dbd45e0f559ca1c82c392ef7a6373a9ccde04a54
Johan Andersson <johan.andersson@lime.tech>
2023-02-22 03:33:04 +0100

    feat(labels): support exclusion regexp (#408)
    * test: make labels test pass again
    
    fixes github/safe-settings#407
    
    * feat(labels): support exclusion regexp
    
    Adds support for alternative labels configuration where
    labels matching a given pattern can be excluded from
    delete operations. This allows other tools to create
    labels without having them deleted by safe-settings.
    
    Example: The following config would keep the labels
    `released`, `released on @dev`, `released on @7.x`
    created by a tool such as Semantic Release.
    
    ```
    labels:
      include:
        - name: bug
          color: '#ff0000'
      exclude:
        - name: ^release
    ```
    
    Include can be used without specifying exclude in the
    config, while also supporting the current format:
    
    ```
    labels:
      - name: bug
        color: '#ff0000'
    ```

--- test/unit/lib/plugins/labels.test.js
+++ test/unit/lib/plugins/labels.test.js
@@ -2,14 +2,19 @@ const Labels = require('../../../../lib/plugins/labels')
 
 describe('Labels', () => {
   let github
+  let log
 
-  function configure (config) {
-    return new Labels(github, { owner: 'bkeepers', repo: 'test' }, config)
+  function configure(config) {
+    const nop = false;
+    return new Labels(nop, github, { owner: 'bkeepers', repo: 'test' }, config, log)
   }
 
   beforeEach(() => {
     github = {
       paginate: jest.fn().mockImplementation(() => Promise.resolve()),
+      repos: {
+        get: jest.fn().mockImplementation(() => Promise.resolve({}))
+      },
       issues: {
         listLabelsForRepo: {
           endpoint: {
@@ -21,6 +26,7 @@ describe('Labels', () => {
         updateLabel: jest.fn().mockImplementation(() => Promise.resolve())
       }
     }
+    log = { debug: jest.fn(), error: console.error }
   })
 
   describe('sync', () => {
@@ -38,7 +44,7 @@ describe('Labels', () => {
         { name: 'new-name', oldname: 'update-me', color: 'FFFFFF', description: '' },
         { name: 'new-color', color: '999999', description: '' },
         { name: 'new-description', color: '#000000', description: 'Hello world' },
-        { name: 'added' }
+        { name: 'added', description: '' }
       ])
 
       return plugin.sync().then(() => {
@@ -53,6 +59,7 @@ describe('Labels', () => {
           owner: 'bkeepers',
           repo: 'test',
           name: 'added',
+          description: '',
           headers: { accept: 'application/vnd.github.symmetra-preview+json' }
         })
 
@@ -88,6 +95,52 @@ describe('Labels', () => {
 
         expect(github.issues.deleteLabel).toHaveBeenCalledTimes(1)
         expect(github.issues.updateLabel).toHaveBeenCalledTimes(3)
+        expect(github.issues.createLabel).toHaveBeenCalledTimes(1)
+      })
+    })
+
+    it('does not delete excluded labels', () => {
+      github.paginate.mockReturnValueOnce(Promise.resolve([
+        { name: 'no-change', color: 'FF0000', description: '' },
+        { name: 'keep-me', color: '000000', description: '' },
+        { name: 'things-to-keep', color: '000000', description: '' },
+        { name: 'released on @7.x.x', color: '000000', description: '' },
+        { name: 'update-me', color: '0000FF', description: '' },
+        { name: 'delete-me', color: '000000', description: '' }
+      ]))
+
+      const plugin = configure({
+        exclude: [
+          { name: 'keep' },
+          { name: '^released' },
+        ],
+        include: [
+          { name: 'no-change', color: 'FF0000', description: '' },
+          { name: 'new-name', oldname: 'update-me', color: 'FFFFFF', description: '' },
+          { name: 'added', description: '' }
+        ]
+      })
+
+      return plugin.sync().then(() => {
+        expect(github.issues.deleteLabel).toHaveBeenCalledWith({
+          owner: 'bkeepers',
+          repo: 'test',
+          name: 'delete-me',
+          headers: { accept: 'application/vnd.github.symmetra-preview+json' }
+        })
+
+        expect(github.issues.updateLabel).toHaveBeenCalledWith({
+          owner: 'bkeepers',
+          repo: 'test',
+          current_name: 'update-me',
+          name: 'new-name',
+          color: 'FFFFFF',
+          description: '',
+          headers: { accept: 'application/vnd.github.symmetra-preview+json' }
+        })
+
+        expect(github.issues.deleteLabel).toHaveBeenCalledTimes(1)
+        expect(github.issues.updateLabel).toHaveBeenCalledTimes(1)
         expect(github.issues.createLabel).toHaveBeenCalledTimes(1)
       })
     })
