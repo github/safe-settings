54c1b813edac5e5b131ad9f1878e60b2fe455e17
Victor Martinez <victormartinezrubio@gmail.com>
2023-06-06 15:19:37 +0200

    Enable back the GitHub pull request (#479)

--- test/unit/lib/env.test.js
+++ test/unit/lib/env.test.js
@@ -20,6 +20,11 @@ describe('env', () => {
       expect(SETTINGS_FILE_PATH).toEqual('deployment-settings.yml')
     })
 
+    it('loads default ENABLE_PR_COMMENT if not passed', () => {
+      const ENABLE_PR_COMMENT = envTest.ENABLE_PR_COMMENT
+      expect(ENABLE_PR_COMMENT).toEqual('false')
+    })
+
   })
 
   describe('load override values', () => {
@@ -29,6 +34,7 @@ describe('env', () => {
       process.env.ADMIN_REPO = '.github'
       process.env.SETTINGS_FILE_PATH = 'safe-settings.yml'
       process.env.DEPLOYMENT_CONFIG_FILE = 'safe-settings-deployment.yml'
+      process.env.ENABLE_PR_COMMENT = 'true'
     })
 
     it('loads override values if passed', () => {
@@ -39,6 +45,8 @@ describe('env', () => {
       expect(SETTINGS_FILE_PATH).toEqual('safe-settings.yml')
       const DEPLOYMENT_CONFIG_FILE = envTest.DEPLOYMENT_CONFIG_FILE
       expect(DEPLOYMENT_CONFIG_FILE).toEqual('safe-settings-deployment.yml')
+      const ENABLE_PR_COMMENT = envTest.ENABLE_PR_COMMENT
+      expect(ENABLE_PR_COMMENT).toEqual('true')
     })
   })
 
