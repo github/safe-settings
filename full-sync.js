f16b324102ce1cf17d5c0c23eef57be3bdfa9e1a
Dolan Antenucci <antenucci.d@gmail.com>
2024-03-06 12:07:41 -0700

    Adding error handling in full-sync.js

--- full-sync.js
+++ full-sync.js
@@ -2,5 +2,18 @@ const { createProbot } = require('probot')
 const appFn = require('./')
 
 const probot = createProbot()
+probot.log.info('Starting full sync.')
 const app = appFn(probot, {})
 app.syncInstallation()
+  .then(settings => {
+    if (settings.errors.length > 0) {
+      probot.log.error('Errors occurred during full sync.')
+      process.exit(1)
+    } else {
+      probot.log.info('Done with full sync.')
+    }
+  })
+  .catch(error => {
+    process.stdout.write(`Unexpected error during full sync: ${error}\n`)
+    process.exit(1)
+  })
